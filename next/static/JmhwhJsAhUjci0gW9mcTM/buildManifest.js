324d4305a643f4192e888c513095b3578557bab3
inceabdullah <inceabdullah@yahoo.com>
2021-06-10 15:06:56 +0300

    Updates

---
+++ next/static/JmhwhJsAhUjci0gW9mcTM/buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/css/e94d35caba235bf04cc6.css","static/css/bfc761d9fd7375cb57e0.css","static/chunks/pages/index-982b3706df18c0acb3dc.js"],"/_error":["static/chunks/pages/_error-e933db6513502c10a07b.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
